3e29fd2202dbd921268e280f456007b46460b419
udaykiriti <123718855+udaykiriti@users.noreply.github.com>
2025-05-23 15:54:49 +0530

    Update FacultyRepository.java

--- src/main/java/com/studentworkshop/repositories/FacultyRepository.java
+++ src/main/java/com/studentworkshop/repositories/FacultyRepository.java
@@ -2,7 +2,6 @@ package com.studentworkshop.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.studentworkshop.models.Faculty;
 
 @Repository
