fcbf61054c6d3bf3ad4f91309c518c7929a6912b
udaykiriti <123718855+udaykiriti@users.noreply.github.com>
2025-05-23 15:58:13 +0530

    Update UserService.java

--- src/main/java/com/studentworkshop/services/UserService.java
+++ src/main/java/com/studentworkshop/services/UserService.java
@@ -2,10 +2,8 @@ package com.studentworkshop.services;
 
 import com.studentworkshop.models.User;
 import com.studentworkshop.repositories.UserRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
