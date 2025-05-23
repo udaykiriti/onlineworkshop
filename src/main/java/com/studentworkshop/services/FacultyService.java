e4d00df360cadc0c500d0a3a1964780979bef4d4
udaykiriti <123718855+udaykiriti@users.noreply.github.com>
2025-05-23 15:56:38 +0530

    Update FacultyService.java

--- src/main/java/com/studentworkshop/services/FacultyService.java
+++ src/main/java/com/studentworkshop/services/FacultyService.java
@@ -2,10 +2,8 @@ package com.studentworkshop.services;
 
 import com.studentworkshop.models.Faculty;
 import com.studentworkshop.repositories.FacultyRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
