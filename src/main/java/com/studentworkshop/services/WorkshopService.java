8f5a6203b5e6f72f57119062334b24451722e57b
udaykiriti <123718855+udaykiriti@users.noreply.github.com>
2025-05-23 15:58:31 +0530

    Update WorkshopService.java

--- src/main/java/com/studentworkshop/services/WorkshopService.java
+++ src/main/java/com/studentworkshop/services/WorkshopService.java
@@ -2,10 +2,8 @@ package com.studentworkshop.services;
 
 import com.studentworkshop.models.Workshop;
 import com.studentworkshop.repositories.WorkshopRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
