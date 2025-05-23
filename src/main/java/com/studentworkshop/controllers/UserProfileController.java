da54ad9fdabc844d779f66c22504be0840acd094
udaykiriti <123718855+udaykiriti@users.noreply.github.com>
2025-05-23 15:52:58 +0530

    Update UserProfileController.java

--- src/main/java/com/studentworkshop/controllers/UserProfileController.java
+++ src/main/java/com/studentworkshop/controllers/UserProfileController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import com.studentworkshop.models.UserProfile;
 import com.studentworkshop.services.UserProfileService;
 
