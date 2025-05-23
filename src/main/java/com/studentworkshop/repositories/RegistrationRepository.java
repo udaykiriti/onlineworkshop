b94d4ba2022c93bd98a11f9a2d31ce3c912e2c7c
udaykiriti <123718855+udaykiriti@users.noreply.github.com>
2025-05-23 15:55:05 +0530

    Update RegistrationRepository.java

--- src/main/java/com/studentworkshop/repositories/RegistrationRepository.java
+++ src/main/java/com/studentworkshop/repositories/RegistrationRepository.java
@@ -2,9 +2,7 @@ package com.studentworkshop.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.studentworkshop.models.Registration;
-
 import java.util.List;
 
 @Repository
